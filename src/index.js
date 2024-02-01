985dd5059fc68206d9f223814d28c402a328c02f
Damian Mostert <damianmostert86@gmail.com>
2024-02-01 18:06:01 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,13 +10,17 @@ import { Popup, Button } from "./components";
 import { Location } from './modules';
 
 
-const backEndUrl = "http://127.0.0.1:8000";
+const backEndUrl = "https://panic-backend-express.vercel.app";
 
 function Main() {
 
   const [tab, setTab] = useState("login");
   const [user, setUser] = useState(null);
   const [tempData, setTempData] = useState(null);
+
+  useEffect(() => {
+    console.log(tempData)
+  }, [tempData]) 
 
   const location = Location();
 
@@ -35,15 +39,15 @@ function Main() {
     });
 
     fetch(backEndUrl + "/api/login", {
-      method: "POST", 
-      mode: "cors", 
-      cache: "no-cache", 
-      credentials: "same-origin", 
+      method: "POST",
+      mode: "cors",
+      cache: "no-cache",
+      credentials: "same-origin",
       headers: {
         "Content-Type": "application/json",
       },
-      redirect: "follow", 
-      referrerPolicy: "no-referrer", 
+      redirect: "follow",
+      referrerPolicy: "no-referrer",
       body: JSON.stringify({
         email: document.getElementById("email").value,
         password: document.getElementById("password").value,
@@ -84,17 +88,17 @@ function Main() {
       icon: "loading",
       background: "blur"
     });
-    
+
     fetch(backEndUrl + "/api/panic-history", {
-      method: "POST", 
-      mode: "cors", 
-      cache: "no-cache", 
-      credentials: "same-origin", 
+      method: "POST",
+      mode: "cors",
+      cache: "no-cache",
+      credentials: "same-origin",
       headers: {
         "Content-Type": "application/json",
       },
-      redirect: "follow", 
-      referrerPolicy: "no-referrer", 
+      redirect: "follow",
+      referrerPolicy: "no-referrer",
       body: JSON.stringify({}),
     }).then(res => res.json()).then(res => {
 
@@ -112,7 +116,7 @@ function Main() {
 
         let newTempData = [];
 
-        for(let item of res.data){
+        for (let item of res.data) {
           let Data = JSON.parse(item.data);
           if (!item.canceled) {
             newTempData.push({
@@ -194,7 +198,7 @@ function Main() {
     const panicType = document.getElementById("panic-type");
     const panicDetails = document.getElementById("panic-details");
 
-    if(!panicType.value || !panicDetails.value){
+    if (!panicType.value || !panicDetails.value) {
       return Popup.fire({
         icon: "unapproved",
         text: "Please select a type and insert details",
@@ -219,25 +223,25 @@ function Main() {
       background: "blur"
     });
     fetch(backEndUrl + "/api/send-panic", {
-      method: "POST", 
-      mode: "cors", 
-      cache: "no-cache", 
-      credentials: "same-origin", 
+      method: "POST",
+      mode: "cors",
+      cache: "no-cache",
+      credentials: "same-origin",
       headers: {
         "Content-Type": "application/json",
       },
-      redirect: "follow", 
-      referrerPolicy: "no-referrer", 
+      redirect: "follow",
+      referrerPolicy: "no-referrer",
       body: JSON.stringify({
-        status:"active",
+        status: "active",
         type: panicType.value,
-        details:panicDetails.value,
-        longitude:location.longitude,
-        latitude:location.latitude,
-        created_by:{
-          id_num:user.id_num,
-          email:user.email,
-          name:user.name,          
+        details: panicDetails.value,
+        longitude: location.longitude,
+        latitude: location.latitude,
+        created_by: {
+          id_num: user.id_num,
+          email: user.email,
+          name: user.name,
         }
       }),
     }).then(res => res.json()).then(res => {
@@ -269,7 +273,7 @@ function Main() {
     })
 
   };
-  
+
   const openCancelPanic = (index) => {
     Popup.fire({
       background: "blur",
@@ -283,6 +287,7 @@ function Main() {
     }).then(res => {
       if (res.confirmed) {
         const panicId = tempData[index].id;
+        console.log(tempData[index]);
         Popup.fire({
           icon: "loading",
           background: "blur"
@@ -296,16 +301,16 @@ function Main() {
             "Content-Type": "application/json",
           },
           redirect: "follow",
-          referrerPolicy: "no-referrer", 
+          referrerPolicy: "no-referrer",
           body: JSON.stringify({ id: panicId, canceled: true }),
         })
-          .then(res =>  res.json())
+          .then(res => res.json())
           .then(response => {
             // Handle the response, update UI, etc.
 
-            if(response.status == "success"){
+            if (response.status == "success") {
               loadPanics();
-            }else{
+            } else {
               return Popup.fire({
                 icon: "unapproved",
                 text: "Failed to cancel panic",
@@ -387,133 +392,136 @@ function Main() {
 
   useEffect(() => { user && setTab("send") }, [user]);
 
-  return <div style={{letterSpacing:"2px"}} className="bg-gray-200 rounded-3xl shadow-2xl p-4 flex flex-wrap">
-    {(() => {
-      switch (tab) {
-        case "login":
-          return <>
-            <div className="w-[300px] flex flex-wrap">
-              <h2 className="text-2xl w-full text-center">
-                LOGIN
-              </h2>
-              <label className="pt-4 ml-1">Email</label>
-              <input type="email" id="email" placeholder='Enter email' className="w-full text-center rounded-xl p-1" />
-              <label className="pt-4 ml-1">Password</label>
-              <input type="password" id="password" placeholder='Enter password' className="w-full text-center mb-4 rounded-xl p-1" />
-              <Button className="m-auto" variant='basic' onClick={login} label="Login" />
-            </div>
-          </>
+  return <>
+    <img src="/batman.png" className='fixed bottom-4 right-4 w-[100px] h-auto' />
+    <div style={{ letterSpacing: "2px", background: "#222" }} className="rounded-3xl shadow-2xl p-4 flex flex-wrap text-yellow-400">
+      {(() => {
+        switch (tab) {
+          case "login":
+            return <>
+              <div className="w-[300px] flex flex-wrap">
+                <h2 className="text-2xl w-full text-center">
+                  LOGIN
+                </h2>
+                <label className="pt-4 ml-1">Email</label>
+                <input type="email" id="email" placeholder='Enter email' className="w-full text-center rounded-xl p-1" />
+                <label className="pt-4 ml-1">Password</label>
+                <input type="password" id="password" placeholder='Enter password' className="w-full text-center mb-4 rounded-xl p-1" />
+                <Button className="m-auto" variant='basic' onClick={login} label="Login" />
+              </div>
+            </>
 
-        case "send":
-          return <div className='flex flex-wrap w-[600px]'>
-            <div className="flex relative items-center w-full bg-white p-4 shadow-md rounded-xl">
-              <div className="absolute w-min left-0">
-                <Button label="panics" onClick={loadPanics} />
-              </div>
-              <h2 className="text-2xl w-full text-center">
-                SEND PANIC
-              </h2>
-              <div className="absolute w-min right-0">
-                <Button label="logout" onClick={logout} />
-              </div>
-            </div>
-            <div className='w-full my-4 flex flex-wrap'>
-              <div className='flex flex-wrap w-full pt-4'>
-                <span className='mr-4 mb-1'>Type</span>
-                <select defaultValue={null} id="panic-type" className='text-center w-full bg-white p-2 rounded-md'>
-                  <option value="">please select an option</option>
-                  <option value="Bank Robery">Bank Robery</option>
-                  <option value="Hitman On The Loose">Hitman On The Loose</option>
-                </select>
-              </div>
-              <div className='flex flex-wrap w-full pt-4'>
-                <span className='mr-4 mb-1'>Details</span>
-                <textarea id="panic-details" placeholder='enter details' className='text-center w-full p-2 rounded-md focus:outline-none' type="text" style={{resize:"none"}}/>
-              </div>
-            </div>
-            <div className='m-auto'>
-              <Button label="send" variant='basic' onClick={sendPanic} />
-            </div>
-            <div className='m-auto'>
-              <Button label="view canceled" variant='basic' onClick={loadCanceledPanics} />
-            </div>
-          </div>
-        case "history":case "canceled":
-          return <div className='flex flex-wrap w-[800px]'>
-            <div className="flex relative items-center w-full bg-white p-4 shadow-md rounded-xl">
-              <div className="absolute w-min left-0">
-                <Button label="back" onClick={closePanics} />
-              </div>
-              <h2 className="text-2xl w-full text-center">
-                {tab == "canceled" && <>CANCELED</>} PANICS
-              </h2>
-              <div className="absolute w-min right-0">
-                <Button label="logout" onClick={logout} />
-              </div>
-            </div>
-            <div className='w-full mt-4 h-[400px] overflow-auto flex flex-wrap'>
-              {tempData && tempData.map((item,key)=>{
-                return <div key={`panic-item-${key}`} className='bg-for-table w-full flex-wrap p-4' >
-                  <div className='my-1 flex flex-wrap w-full' style={{ borderBottom: "2px dotted #CCC" }}>
-                    <div className='w-1/2'>
-                      Type
-                    </div>
-                    <div className='w-1/2'>
-                      {item.type}
-                    </div>
-                 </div>
-                  <div className='my-1 flex flex-wrap w-full' style={{ borderBottom: "2px dotted #CCC" }}>
-                    <div className='w-1/2'>
-                      Details
-                    </div>
-                    <div className='w-1/2 mb-1'>
-                      {item.details}
-                    </div>
-                  </div>
-                  <div className='my-1 flex flex-wrap w-full' style={{ borderBottom: "2px dotted #CCC" }}>
-                    <div className='w-1/2'>
-                      Longitude
-                    </div>
-                    <div className='w-1/2'>
-                      {item.longitude}
-                    </div>
-                  </div>
-                  <div className='my-1 flex flex-wrap w-full' style={{ borderBottom: "2px dotted #CCC" }}>
-                    <div className='w-1/2'>
-                      Latitude
-                    </div>
-                    <div className='w-1/2'>
-                      {item.latitude}
-                    </div>
-                  </div>
-                  <div className='my-1 flex flex-wrap w-full' style={{ borderBottom: "2px dotted #CCC" }}>
-                    <div className='w-1/2'>
-                      Created at
-                    </div>
-                    <div className='w-1/2'>
-                      {item.created_at}
-                    </div>
-                  </div>
-                  <div className='my-1 flex flex-wrap w-full' style={{ borderBottom: "2px dotted #CCC" }}>
-                    <div className='w-1/2'>
-                      Updated at
-                    </div>
-                    <div className='w-1/2'>
-                      {item.updated_at}
-                    </div>
-                  </div>
-                  <div className='w-full flex justify-end mt-4'>
-                    <Button onClick={() => tab == "canceled" ? openUnCancelPanic(key) : openCancelPanic(key)} variant='basic' label={tab == "canceled" ? "uncancel" : "cancel"}/>
-                  </div>
+          case "send":
+            return <div className='flex flex-wrap w-[600px] '>
+              <div className="flex relative items-center w-full bg-black  p-4 shadow-md rounded-xl">
+                <div className="absolute w-min left-0">
+                  <Button label="panics" onClick={loadPanics} />
                 </div>
-              })}
+                <h2 className="text-2xl w-full text-center">
+                  SEND PANIC
+                </h2>
+                <div className="absolute w-min right-0">
+                  <Button label="logout" onClick={logout} />
+                </div>
+              </div>
+              <div className='w-full my-4 flex flex-wrap'>
+                <div className='flex flex-wrap w-full pt-4'>
+                  <span className='mr-4 mb-1'>Type</span>
+                  <select defaultValue={null} id="panic-type" className='text-center w-full bg-black p-2 rounded-md'>
+                    <option value="">please select an option</option>
+                    <option value="Bank Robery">Bank Robery</option>
+                    <option value="Hitman On The Loose">Hitman On The Loose</option>
+                  </select>
+                </div>
+                <div className='flex flex-wrap w-full pt-4'>
+                  <span className='mr-4 mb-1'>Details</span>
+                  <textarea id="panic-details" placeholder='enter details' className='text-center w-full p-2 rounded-md focus:outline-none bg-black' type="text" style={{ resize: "none" }} />
+                </div>
+              </div>
+              <div className='m-auto'>
+                <Button label="send" variant='basic' onClick={sendPanic} />
+              </div>
+              <div className='m-auto'>
+                <Button label="view canceled" variant='basic' onClick={loadCanceledPanics} />
+              </div>
             </div>
-          </div>
-        default:
-          return <></>
-      }
-    })()}
-  </div>
+          case "history": case "canceled":
+            return <div className='flex flex-wrap w-[800px]'>
+              <div className="flex relative items-center w-full bg-black p-4 shadow-md rounded-xl">
+                <div className="absolute w-min left-0">
+                  <Button label="back" onClick={closePanics} />
+                </div>
+                <h2 className="text-2xl w-full text-center">
+                  {tab == "canceled" && <>CANCELED</>} PANICS
+                </h2>
+                <div className="absolute w-min right-0">
+                  <Button label="logout" onClick={logout} />
+                </div>
+              </div>
+              <div className='w-full mt-4 h-[400px] overflow-auto flex flex-wrap'>
+                {tempData && tempData.map((item, key) => {
+                  return <div key={`panic-item-${key}`} className='bg-for-table w-full flex-wrap p-4' >
+                    <div className='py-1 flex flex-wrap w-full' style={{ borderBottom: "2px dotted yellow" }}>
+                      <div className='w-1/2'>
+                        Type
+                      </div>
+                      <div className='w-1/2'>
+                        {item.type}
+                      </div>
+                    </div>
+                    <div className='py-1 flex flex-wrap w-full' style={{ borderBottom: "2px dotted yellow" }}>
+                      <div className='w-1/2'>
+                        Details
+                      </div>
+                      <div className='w-1/2 mb-1'>
+                        {item.details}
+                      </div>
+                    </div>
+                    <div className='py-1 flex flex-wrap w-full' style={{ borderBottom: "2px dotted yellow" }}>
+                      <div className='w-1/2'>
+                        Longitude
+                      </div>
+                      <div className='w-1/2'>
+                        {item.longitude}
+                      </div>
+                    </div>
+                    <div className='py-1 flex flex-wrap w-full' style={{ borderBottom: "2px dotted yellow" }}>
+                      <div className='w-1/2'>
+                        Latitude
+                      </div>
+                      <div className='w-1/2'>
+                        {item.latitude}
+                      </div>
+                    </div>
+                    <div className='py-1 flex flex-wrap w-full' style={{ borderBottom: "2px dotted yellow" }}>
+                      <div className='w-1/2'>
+                        Created at
+                      </div>
+                      <div className='w-1/2'>
+                        {item.created_at}
+                      </div>
+                    </div>
+                    <div className='py-1 flex flex-wrap w-full' style={{ borderBottom: "2px dotted yellow" }}>
+                      <div className='w-1/2'>
+                        Updated at
+                      </div>
+                      <div className='w-1/2'>
+                        {item.updated_at}
+                      </div>
+                    </div>
+                    <div className='w-full flex justify-end mt-4'>
+                      <Button onClick={() => tab == "canceled" ? openUnCancelPanic(key) : openCancelPanic(key)} variant='basic' label={tab == "canceled" ? "uncancel" : "cancel"} />
+                    </div>
+                  </div>
+                })}
+              </div>
+            </div>
+          default:
+            return <></>
+        }
+      })()}
+    </div>
+  </>
 }
 
 ReactDOM.createRoot(document.getElementById('root')).render(<React.StrictMode><Main /><Popup /></React.StrictMode>);
